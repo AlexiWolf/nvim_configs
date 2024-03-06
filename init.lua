0a04277e86d9cb95a276f847788fd731861fc260
AlexiWolf <alexi@wolvesin.space>
2024-03-06 17:01:57 -0600

    Changed theme to `duskfox`

--- init.lua
+++ init.lua
@@ -375,11 +375,11 @@ require('lazy').setup {
   },
 
   { -- You can easily change to a different colorscheme.
-    'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'duskfox'
 
       vim.cmd.hi 'Comment gui=none'
     end,

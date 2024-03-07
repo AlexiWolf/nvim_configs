9145e38922b321faa84784c5dfa5537d9dfd1402
AlexiWolf <alexi@wolvesin.space>
2024-03-06 21:39:45 -0600

    Added keybind to open Which Key menu with backspace

--- init.lua
+++ init.lua
@@ -119,6 +119,9 @@ require('lazy').setup {
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
+      -- Display Which-Key menu
+      vim.keymap.set('n', '<backspace>', ':WhichKey<cr>', { desc = 'Open Whichkey menu' })
+
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },

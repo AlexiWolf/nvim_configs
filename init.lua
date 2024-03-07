440b775d92bb3d21780d070d44d6d2ffa08d1a2d
AlexiWolf <alexi@wolvesin.space>
2024-03-07 02:43:29 -0600

    Added `fzf` config

--- init.lua
+++ init.lua
@@ -153,6 +153,12 @@ require('lazy').setup {
       -- [[ Configure Telescope ]]
       require('telescope').setup {
         extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },

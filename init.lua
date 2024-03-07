4d2c2b3a1c5eb913b3a593df4e6e5bd1c1f4e821
AlexiWolf <alexi@wolvesin.space>
2024-03-06 21:39:21 -0600

    Added color columns

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+vim.opt.colorcolumn = { 79, 99, 119 }
 
 -- [[ Basic Keymaps ]]
 

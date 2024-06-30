cbbd5192449e5cba214b6035b06ce4d242a526dc
AlexiWolf <alexi@wolvesin.space>
2024-06-30 10:37:54 -0500

    Remove comment

--- init.lua
+++ init.lua
@@ -325,8 +325,8 @@ require('lazy').setup {
       require('mason').setup()
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format lua code
         'rust-analyzer',
+        'stylua',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

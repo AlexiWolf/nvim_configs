e5852c17ccb852267263b8037b9b1e85c9d85289
AlexiWolf <alexi@wolvesin.space>
2025-02-21 14:59:20 -0600

    Remove noise comments / update plugin config style

--- init.lua
+++ init.lua
@@ -91,14 +91,18 @@ vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup {
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'gpanders/editorconfig.nvim', -- Support for .editorconfig files.
+
+  -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' },
+
+  -- Support for .editorconfig files.
+  { 'gpanders/editorconfig.nvim' },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim' },
 
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
@@ -111,10 +115,11 @@ require('lazy').setup {
     },
   },
 
-  { -- Useful plugin to show you pending keybinds.
+  -- Useful plugin to show you pending keybinds
+  {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
+    event = 'VimEnter',
+    config = function()
       require('which-key').setup()
 
       -- Display Which-Key menu
@@ -133,34 +138,31 @@ require('lazy').setup {
     end,
   },
 
+  -- Fuzzy Finder for searching through files, lsp symbols, and more
   {
     'ibhagwan/fzf-lua',
-    -- optional for icon support
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional for icon support
     config = function()
-      -- Setup
       local fzf = require 'fzf-lua'
       fzf.setup()
-
-      -- Keybindings
       vim.keymap.set('n', '<leader>f', fzf.files, { desc = '[F]ile Search' })
       vim.keymap.set('n', '<leader>t', fzf.tags, { desc = '[T]ag Search' })
     end,
   },
 
+  -- Automatically generate tags files
   { 'ludovicchabant/vim-gutentags' },
 
-  { -- LSP Configuration & Plugins
+  -- LSP Configuration & Plugins
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
       -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim' },
     },
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
@@ -177,13 +179,11 @@ require('lazy').setup {
           map('gr', fzf.lsp_references, '[G]o to [R]eferences')
           map('gI', fzf.lsp_implementations, '[G]o to [I]mplementation')
           map('gD', vim.lsp.buf.declaration, '[G]o to [D]eclaration')
-
           map('<leader>ws', fzf.lsp_workspace_symbols, '[W]orkspace [S]ymbols Search')
           map('<leader>bs', fzf.lsp_document_symbols, '[B]uffer [S]ymbols Search')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
           map('<leader>th', function()
             local is_enabled = not vim.lsp.inlay_hint.is_enabled()
             vim.lsp.inlay_hint.enable(is_enabled)
@@ -205,7 +205,6 @@ require('lazy').setup {
               buffer = event.buf,
               callback = vim.lsp.buf.document_highlight,
             })
-
             vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
               buffer = event.buf,
               callback = vim.lsp.buf.clear_references,
@@ -217,7 +216,7 @@ require('lazy').setup {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- Enable the following language servers
+      -- Enable the following language servers.
       local servers = {
         pyright = {},
         lua_ls = {
@@ -239,24 +238,23 @@ require('lazy').setup {
         },
       }
 
-      -- Setup / install tools from Mason
+      -- Setup / install devtools with Mason.
       require('mason').setup()
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
       require('mason-lspconfig').setup {
         automatic_installation = {},
         ensure_installed = {},
         handlers = {
           function(server_name)
-            if server_name ~= 'rust_analyzer' then
+            if server_name ~= 'rust_analyzer' then -- Skip setup for rust-analzyer to avoid conflict with rustaceanvim.
               local server = servers[server_name] or {}
               -- This handles overriding only values explicitly passed
               -- by the server configuration above. Useful when disabling
-              -- certain features of an LSP (for example, turning off formatting for tsserver)
+              -- certain features of an LSP (for example, turning off formatting for tsserver.)
               server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
               require('lspconfig')[server_name].setup(server)
             end
@@ -265,9 +263,11 @@ require('lazy').setup {
       }
     end,
   },
+
+  -- Rust language support.
   {
     'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
+    version = '^5',
     event = 'VimEnter',
     config = function()
       vim.g.rustaceanvim = {
@@ -285,7 +285,9 @@ require('lazy').setup {
       }
     end,
   },
-  { -- Autoformat
+
+  -- Autoformat.
+  {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
@@ -300,17 +302,18 @@ require('lazy').setup {
     },
   },
 
-  { -- Autocompletion
+  -- Autocompletion.
+  {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
+      -- Snippet Engine & its associated nvim-cmp source.
       {
         'L3MON4D3/LuaSnip',
         build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
           if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
             return
           end
@@ -323,7 +326,7 @@ require('lazy').setup {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
 
-      -- Pre-configured snippets
+      -- Pre-configured snippets.
       'rafamadriz/friendly-snippets',
     },
     config = function()
@@ -343,7 +346,7 @@ require('lazy').setup {
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-Space>'] = cmp.mapping.complete {}, -- Manually trigger a completion from nvim-cmp.
+          ['<C-Space>'] = cmp.mapping.complete {},
 
           ['<C-l>'] = cmp.mapping(function()
             if luasnip.expand_or_locally_jumpable() then
@@ -365,9 +368,10 @@ require('lazy').setup {
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
+  -- Colorscheme.
+  {
     'EdenEast/nightfox.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
       transparent = true,
